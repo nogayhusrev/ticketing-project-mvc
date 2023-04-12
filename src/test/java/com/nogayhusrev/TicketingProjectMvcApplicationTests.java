7024dcd4abb06a4e782fb6a73d4878f98cbc40de
nogayhusrev <nogayhusrev@gmail.com>
2023-04-12 14:29:37 +0300

    Initial Commit

---
+++ src/test/java/com/nogayhusrev/TicketingProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nogayhusrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
